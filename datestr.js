d0658e08a01e2a650c6389422fe3cecd8874c490
Jiangning Huang <hjnyll@163.com>
2017-02-07 15:32:08 +0800

    fixed mileseconds compute

--- datestr.js
+++ datestr.js
@@ -16,44 +16,43 @@ Date.prototype.toCommonSay = function() {
     var isAm = H <= 12 ? true : false;
     var h = isAm ? H : (H - 12);
     var m = this.getMinutes();
+    var d = this.getDay();
     var d1 = parseInt(ctime / (24 * 3600 * 1000), 10);
     var d2 = parseInt(time / (24 * 3600 * 1000), 10);
-    var gap_day = d1 - d2;
-
-    var d = this.getDay();
+    var gap_day = d1 - d2; // 相差的天数，正数为前，负数为后
+    var gap_time = parseInt((ctime - time) / 1000, 10); // 相差的秒数
 
     var datestr;
-    var gap_time = ctime - time;
-    timestr = (isAm ? '上午' : '下午') + h + ':' + m;
+    var timestr = (isAm ? '上午' : '下午') + h + ':' + m;
 
     if (gap_time > 0) {
-        if (gap_time < 60 * 1000) {
-            datestr = timestr;
-        } else if (gap_time < 3600 * 1000) {
-            datestr = parseInt(gap_time / (60 * 1000), 10) + '分钟前 ' + timestr;
+        if (gap_time < 60) {
+            datestr = '';
+        } else if (gap_time < 3600) {
+            datestr = parseInt(gap_time / 60, 10) + '分钟前 ';
         } else if (gap_day === 0) {
-            datestr = parseInt(gap_time / (3600 * 1000), 10) + '小时前 ' + timestr;
+            datestr = parseInt(gap_time / 3600, 10) + '小时前 ';
         } else if (gap_day === 1) {
-            datestr = '昨日 ' + timestr;
+            datestr = '昨日 ';
         } else if (gap_day < 7) {
-            datestr = day_of_week_str[d] + ' ' + timestr;
+            datestr = day_of_week_str[d] + ' ';
         } else {
-            datestr = this.getFullYear() + '-' + (this.getMonth() + 1) + '-' + this.getDate() + ' ' + timestr;
+            datestr = this.getFullYear() + '-' + (this.getMonth() + 1) + '-' + this.getDate() + ' ';
         }
     } else {
         gap_time = 0 - gap_time;
         gap_day = 0 - gap_day;
-        if (gap_time < 60 * 1000) {
-            datestr = timestr;
-        } else if (gap_time < 3600 * 1000) {
-            datestr = parseInt(gap_time / (60 * 1000), 10) + '分钟后 ' + timestr;
+        if (gap_time < 60) {
+            datestr = '';
+        } else if (gap_time < 3600) {
+            datestr = parseInt(gap_time / 60, 10) + '分钟后 ';
         } else if (gap_day === 0) {
-            datestr = parseInt(gap_time / (3600 * 1000), 10) + '小时后 ' + timestr;
+            datestr = parseInt(gap_time / 3600, 10) + '小时后 ';
         } else if (gap_day === 1) {
-            datestr = '明日 ' + timestr;
+            datestr = '明日 ';
         } else {
-            datestr = this.getFullYear() + '-' + (this.getMonth() + 1) + '-' + this.getDate() + ' ' + timestr;
+            datestr = this.getFullYear() + '-' + (this.getMonth() + 1) + '-' + this.getDate() + ' ';
         }
     }
-    return datestr;
+    return datestr + timestr;
 }
